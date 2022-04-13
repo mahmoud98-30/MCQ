600f57b0afe48d1a8afece5f3060611dc4002755
mahmoudfw <malforwi@becom.ly>
2022-04-13 21:52:26 +0200

    fixed correction

--- paper/correction/excel.py
+++ paper/correction/excel.py
@@ -2,8 +2,10 @@ import io
 from django.http import HttpResponse
 import xlsxwriter
 
+from paper.correction.parameters import questions, CorrectScore, rect_num, widthImg, heightImg
 
-def ExportExcel(TotelGRADING, TotelScore, PercentScore, CorrectAns, StudentAns):
+
+def ExportExcel(GRADINGLIST, FinelScore, CorrectAns, StudentAns, num_of_students_paper):
     # Create an in-memory output file for the new workbook.
     output = io.BytesIO()
 
@@ -11,89 +13,91 @@ def ExportExcel(TotelGRADING, TotelScore, PercentScore, CorrectAns, StudentAns):
     workbook = xlsxwriter.Workbook(output)
     sheet = workbook.add_worksheet()
 
+    # TotelGRADING = [item for sublist in GRADINGLIST for item in sublist]
+    # print(TotelGRADING)
+
     # data
     print('##################################')
-    print("TOTEL CORRECTION", TotelGRADING)
-    print("TOTEL SCORE", TotelScore)
-    print("Finel SCORE", PercentScore)
+    print("TOTEL CORRECTION", GRADINGLIST)
+    print("Finel Score", FinelScore)
     print("Correct Answer", CorrectAns)
     print("Student Answer", StudentAns)
     print('##################################')
 
-    # Header
-    student_name = "Student Name"
-    No_que = "Number Of Question"
-    result = "Result"
-    stu_answer = "Student Answer"
-    corr_ans = "Correct Answer"
-    result_percent = "Result By Percent"
+    # # Header
+    # student_name = "Student Name"
+    # No_que = "Number Of Question"
+    # result = "Result"
+    # stu_answer = "Student Answer"
+    # corr_ans = "Correct Answer"
+    # result_percent = "Result By Percent"
+    #
+    # sheet.write("A1", "??? ??????")
+    # sheet.write("A2", "??? ??????")
+    # sheet.write("A3", "??????")
+    # sheet.write("A4", "??????? ??????")
+    # sheet.write("A5", "??????? ???????")
+    # sheet.write("A7", "???? ???????")
 
-    sheet.write("A1", "??? ??????")
-    sheet.write("A2", "??? ??????")
-    sheet.write("A3", "??????")
-    sheet.write("A4", "??????? ??????")
-    sheet.write("A5", "??????? ???????")
-    sheet.write("A7", "???? ???????")
-
-    que_num = []
-    for num in range(1, 101):
-        que_num.append(num)
-
-    correct = "Correct"
-    wrong = "Wrong"
-    GRADING = []
-    for gra in TotelGRADING:
-        if gra == 0:
-            GRADING.append("???")
-        else:
-            GRADING.append("????")
-    # print(GRADING)
-
-    Correct = []
-    for cor in CorrectAns:
-        if cor == 0:
-            Correct.append("A")
-        elif cor == 1:
-            Correct.append("B")
-        elif cor == 2:
-            Correct.append("C")
-        else:
-            Correct.append("D")
-    # print(Correct)
-
-    Student = []
-    for stu in StudentAns:
-        if stu == 0:
-            Student.append("A")
-        elif stu == 1:
-            Student.append("B")
-        elif stu == 2:
-            Student.append("C")
-        else:
-            Student.append("D")
-    # print(Student)
-
-    # write data in file
-    for item in range(len(que_num)):
-        sheet.write(1, item + 1, que_num[item])
-        sheet.write(2, item + 1, GRADING[item])
-        sheet.write(3, item + 1, Student[item])
-        sheet.write(4, item + 1, Correct[item])
-
-    sheet.write("A8", PercentScore)
-
-    # Close the workbook before sending the data.
-    workbook.close()
-
-    # Rewind the buffer.
-    output.seek(0)
-
-    # Set up the Http response.
-    filename = 'Result.xlsx'
-    response = HttpResponse(
-        output,
-        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
-    response['Content-Disposition'] = 'attachment; filename=%s' % filename
-
-    return response
+    # que_num = []
+    # for num in range(1, 101):
+    #     que_num.append(num)
+    #
+    # correct = "Correct"
+    # wrong = "Wrong"
+    # GRADING = []
+    # for gra in TotelGRADING:
+    #     if gra == 0:
+    #         GRADING.append("???")
+    #     else:
+    #         GRADING.append("????")
+    # # print(GRADING)
+    #
+    # Correct = []
+    # for cor in CorrectAns:
+    #     if cor == 0:
+    #         Correct.append("A")
+    #     elif cor == 1:
+    #         Correct.append("B")
+    #     elif cor == 2:
+    #         Correct.append("C")
+    #     else:
+    #         Correct.append("D")
+    # # print(Correct)
+    #
+    # Student = []
+    # for stu in StudentAns:
+    #     if stu == 0:
+    #         Student.append("A")
+    #     elif stu == 1:
+    #         Student.append("B")
+    #     elif stu == 2:
+    #         Student.append("C")
+    #     else:
+    #         Student.append("D")
+    # # print(Student)
+    #
+    # # write data in file
+    # for item in range(len(que_num)):
+    #     sheet.write(1, item + 1, que_num[item])
+    #     sheet.write(2, item + 1, GRADING[item])
+    #     sheet.write(3, item + 1, Student[item])
+    #     sheet.write(4, item + 1, Correct[item])
+    #
+    # sheet.write("A8", PercentScore)
+    #
+    # # Close the workbook before sending the data.
+    # workbook.close()
+    #
+    # # Rewind the buffer.
+    # output.seek(0)
+    #
+    # # Set up the Http response.
+    # filename = 'Result.xlsx'
+    # response = HttpResponse(
+    #     output,
+    #     content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    # )
+    # response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    #
+    # return response
--- paper/correction/finel_score.py
+++ paper/correction/finel_score.py
@@ -16,13 +16,15 @@ def finel_correction(CorrectAnslist, StudentAnslist, SimgWarpColored, Simg, pts1
     # COMPARE THE VALUES TO FIND THE CORRECT ANSWERS
     TotelScore = []
     FinelScore = []
+
     for i in range(0, num_of_students_paper):
         c1 = CorrectAnslist
         s1 = StudentAnslist[i]
         # print("CorrectAnslist", c1)
         # print("StudentAnslist", s1)
         # print("-"*100)
-        print("-" * 100)
+        GRADINGLIST = []
+        GRADING = []
         for j in range(0, rect_num):
             c2 = c1[j]
             s2 = s1[j]
@@ -37,60 +39,42 @@ def finel_correction(CorrectAnslist, StudentAnslist, SimgWarpColored, Simg, pts1
                     grading.append(0)
                 # print("c", c2[k], "s", s2[k])
                 # print("-" * 100)
-
-            # utlis.showAnswers(SimgWarpColored, c1[j], grading,
-            #                  s1[j])  # DRAW DETECTED ANSWERS
-            imgRawDrawings = np.zeros_like(SimgWarpColored)  # NEW BLANK IMAGE WITH WARP IMAGE SIZE
-            imgRawDrawings = utlis.showAnswers(imgRawDrawings, c1[j], grading, s1[j])  # DRAW ON NEW IMAGE
-            invMatrix = cv2.getPerspectiveTransform(pts2, pts1)  # INVERSE TRANSFORMATION MATRIX
-            print(pts2, pts1)
-            imgInvWarp = cv2.warpPerspective(imgRawDrawings, invMatrix, (widthImg, heightImg))  # INV IMAGE WARP
-            imgFinal = Simg.copy()
-            imgFinal = cv2.addWeighted(imgFinal, 1, imgInvWarp, 1, 0)
-            # utlis.drawGrid(SimgWarpColored)  # DRAW GRID
-
-            img = cv2.resize(imgFinal, (800, 800))  # RESIZE IMAGE
-            cv2.imshow("SimgWarpColored", img)
-            cv2.waitKey(0)
+            # print("grading",grading)
+            # imgRawDrawings = np.zeros_like(SimgWarpColored)  # NEW BLANK IMAGE WITH WARP IMAGE SIZE
+            # imgRawDrawings = utlis.showAnswers(imgRawDrawings, c1[j], grading, s1[j])  # DRAW ON NEW IMAGE
+            # invMatrix = cv2.getPerspectiveTransform(pts2, pts1)  # INVERSE TRANSFORMATION MATRIX
+            # print(pts2, pts1)
+            # imgInvWarp = cv2.warpPerspective(imgRawDrawings, invMatrix, (widthImg, heightImg))  # INV IMAGE WARP
+            # imgFinal = Simg.copy()
+            # imgFinal = cv2.addWeighted(imgFinal, 1, imgInvWarp, 1, 0)
+            #
+            # img = cv2.resize(imgFinal, (800, 800))  # RESIZE IMAGE
+            # cv2.imshow("SimgWarpColored", img)
+            # cv2.waitKey(0)
 
             # print("c1[j] ", c1[j])
             # print("s1[j] ", s1[j])
-            # print("GRADING ", grading)
+            GRADING.append(grading)
+
             score = (sum(grading) / questions) * 100  # FINAL GRADE
             TotelScore.append(score)
             # print("SCORE", score)
 
+        print("GRADING ", GRADING)
+        GRADINGLIST.append(GRADING)
         PercentScore = (sum(TotelScore) / CorrectScore) * 100
+
         FinelScore.append(PercentScore)
 
         # TODO : FinelScore[i] include all final scores in one list
-
         # print("Finel SCORE", FinelScore[0], "%")
 
 
-
-    # DISPLAYING CORRACT
-    #ResultImg = CimgWarpColored.copy()
-    # ResultImg = utlis.showAnswers(SimgWarpColored, CorrectIndex, grading, StudentIndex)  # DRAW DETECTED ANSWERS
-    # utlis.drawGrid(SimgWarpColored)  # DRAW GRID
-    # imgRawDrawings = np.zeros_like(imgWarpColored)  # NEW BLANK IMAGE WITH WARP IMAGE SIZE
-    # utlis.showAnswers(imgRawDrawings, myIndex, grading, ans)  # DRAW ON NEW IMAGE
-    # invMatrix = cv2.getPerspectiveTransform(pts2, pts1)  # INVERSE TRANSFORMATION MATRIX
-    # imgInvWarp = cv2.warpPerspective(imgRawDrawings, invMatrix, (widthImg, heightImg))  # INV IMAGE WARP
-
-    # blankImg = np.zeros_like(Cimg)
-    # imageArray = ([CimgBigContour, SimgBigContour, CimgThresh, SimgCanny, ])
-    # stackedImage = utlis.stackImages(imageArray, 0.5)
-    # cv2.imshow("Result", stackedImage)
-    # cv2.waitKey(0)
-
-
-
-    # # put many list from one list ,to one list
+    # put many list from one list ,to one list
     # TotelGRADING = [item for sublist in GRADINGLIST for item in sublist]
-    # CorrectAns = [item for sublist in CorrectAnslist for item in sublist]
-    # StudentAns = [item for sublist in StudentAnslist for item in sublist]
-    #
-    # # exoprt to excel file
-    # excel_file = ExportExcel(TotelGRADING, TotelScore, PercentScore, CorrectAns, StudentAns)
-    # return excel_file
+    CorrectAns = [item for sublist in CorrectAnslist for item in sublist]
+    StudentAns = [item for sublist in StudentAnslist for item in sublist]
+
+    # exoprt to excel file
+    excel_file = ExportExcel(GRADINGLIST, FinelScore, CorrectAns, StudentAnslist, num_of_students_paper)
+    return excel_file
